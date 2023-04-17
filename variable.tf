8a58ef9c02e0d3837e4f3d3520fc57a8da74c993
MEGHA0287 <122254451+MEGHA0287@users.noreply.github.com>
2023-04-17 22:43:59 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -15,7 +15,7 @@ variable "instance_type" {
 
 variable "security_group" {
   description = "Name of security group"
-  default     = "sg-0f84ca1b903249877"
+  default     = "0f84ca1b903249877"
 }
 
 variable "tag_name" {
