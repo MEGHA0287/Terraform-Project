f7f6605a085866ac8667145fcbe292b06bde9367
MEGHA0287 <122254451+MEGHA0287@users.noreply.github.com>
2023-04-18 13:49:47 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,11 +1,11 @@
 variable "aws_region" {
   description = "The AWS region to create things in."
-  default     = "us-east-1"
+  default     = "ap-northeast-1"
 }
 
 variable "key_name" {
   description = " SSH keys to connect to ec2 instance"
-  default     = "b-16"
+  default     = "t16"
 }
 
 variable "instance_type" {
@@ -24,7 +24,7 @@ variable "tag_name" {
 }
 variable "ami_id" {
   description = "AMI for Ubuntu Ec2 instance"
-  default     = "ami-007855ac798b5175e"
+  default     = "ami-0d979355d03fa2522"
 }
 variable "versioning" {
   type        = bool
@@ -39,7 +39,7 @@ variable "acl" {
 variable "bucket_prefix" {
   type        = string
   description = "(required since we are not using 'bucket') Creates a unique bucket name beginning with the specified prefix"
-  default     = "mydev-tf-state-bucket-2"
+  default     = "mydev-tf-state-bucket-4"
 }
 variable "tags" {
   type        = map(any)
